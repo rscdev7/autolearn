a3a4c460ad4776ab69a5e67f583c0682a1d7f07f
rosario <rscdev777@outlook.com>
2021-07-23 20:53:01 +0200

    migliorata gestione eccezioni NetworkSerializer

--- code/classes/NetworkSerializer/changelog/changelog.md
+++ code/classes/NetworkSerializer/changelog/changelog.md
@@ -1,4 +1,7 @@
 # NetworkSerializer
 
+> **v1.0.1**
+>	* Migliorata gestione eccezioni.
+
 > **v1.0.0**
 >	* Costruzione compoenente
--- code/classes/NetworkSerializer/test/test_network_serializer.py
+++ code/classes/NetworkSerializer/test/test_network_serializer.py
@@ -1,13 +1,13 @@
 """
 @author           	:  rscalia
 @build-date         :  Thu 15/07/2021
-@last_update        :  Thu 15/07/2021
+@last_update        :  Fri 23/07/2021
 
 Questo componente permette di testare il componente NetworkSerializer
 """
 
 from ..lib.network_serializer.NetworkSerializer import NetworkSerializer
-
+from typing                                     import Union
 
 KEY_PATH:str        = "../data/"
 READ_FROM_DISK:bool = True
@@ -18,25 +18,32 @@ DATA:dict           = {"ciao":450 , "test":"test", "jhg":[4,5,8,7]}
 def test_nt_ser():
     ns:NetworkSerializer = NetworkSerializer(KEY_PATH)
 
+    #Prelievo chiave crittografica
     if (READ_FROM_DISK == False):
-        ns.buildNewKey()
+        res:Union[None, Exception] = ns.buildNewKey()
+        assert issubclass (type(res) , Exception ) == False
     else:
-        ns.readKeyFromFile()
+        res:Union[None, Exception] = ns.readKeyFromFile()
+        assert issubclass (type(res) , Exception ) == False
 
-    res:str     = ns.encryptField(DATA_TO_CYPHER)
-    res:str     = ns.decryptField(res)
 
-    print ("\n[!] Dati decifrati: {}".format(res))
+    res:Union[str, Exception]     = ns.encryptField(DATA_TO_CYPHER)
+    assert issubclass (type(res) , Exception ) == False
+
+    res:Union[str, Exception]     = ns.decryptField(res)
+    assert issubclass (type(res) , Exception ) == False
+
     assert DATA_TO_CYPHER == res
+    print ("\n[!] Dati decifrati: {}".format(res))
 
 
 def test_json_encoding ():
     ns:NetworkSerializer = NetworkSerializer()
 
-    encoded_data:bytes = ns.encodeJson(DATA)
-
-    decoded_data:dict  = ns.decodeJson(encoded_data)
-
-    print ("\n[!] Dati Decodificati: {}".format(decoded_data))
+    encoded_data:bytes  = ns.encodeJson(DATA)
+    decoded_data:dict   = ns.decodeJson(encoded_data)
 
     assert decoded_data == DATA
+    print ("\n[!] Dati Decodificati: {}".format(decoded_data))
+
+    
